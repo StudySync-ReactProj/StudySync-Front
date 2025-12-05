ae1de589a264832db8ab403de0817100d48aa080
S22102001 <Shahar.girtler@gmail.com>
2025-12-05 12:25:53 +0200

    feat: MainTitle

---
+++ studysync_front/src/components/MainTitle/MainTitle.style.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+    color: #0D3682;
+    text-align: left;
+    font-family: Prompt;
+    font-size: 50px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    padding-left: 16px;
+    padding-top: 23px;
+    padding-button: 17px;
+`;
+
