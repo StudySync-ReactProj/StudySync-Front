34b2bdd253c37461fe18f7d831ad5955601d0ce3
Noam-sin <noamsinay9907@gmail.com>
2025-12-19 09:32:02 +0200

    feat: protected routes :))))))))) +  auth-based redirects

--- studysync_front/src/App.jsx
+++ studysync_front/src/App.jsx
@@ -1,6 +1,8 @@
 import Theme from "./MuiTheme.style.js";
 import { ThemeProvider } from "@mui/material/styles";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import { useUser } from "./Contex/UserContex";
 
 import "./App.css";
 import Dashboard from "./pages/Dashboard/Dashboard.jsx";
@@ -8,18 +10,61 @@ import Login from "./pages/Login/Login.jsx";
 import TasksPage from "./pages/TasksPage/TasksPage.jsx";
 import PageNotFound from "./pages/PageNotFound/PageNotFound.jsx";
 
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useUser();
+
+  // if not logined in redirect to login
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+  // if logged in redirect to dashboard
+  return children;
+}
+
+
 function App() {
+  const { isAuthenticated } = useUser();
+
   return (
     <ThemeProvider theme={Theme}>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/Login" element={<Login />} />
-        <Route path="/TasksPage" element={<TasksPage />} />
+
+        {/* Redirect based on authentication status */}
+        <Route
+          path="/"
+          element={
+            isAuthenticated
+              ? <Navigate to="/dashboard" replace />
+              : <Navigate to="/login" replace />
+          }
+        />
+
+        {/* Login route */}
+        <Route path="/login" element={<Login />} />
+
+        {/* Protected routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        {/* Tasks page route */}
+        <Route
+          path="/TasksPage"
+          element={<ProtectedRoute>
+            <TasksPage />
+          </ProtectedRoute>
+          }
+        />
+
+        {/* 404 */}
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </ThemeProvider>
   );
 }
-
-
 export default App;
